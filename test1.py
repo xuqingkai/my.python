ad98a3f094d17ca766d57e88ef23ca1cd4335f01
徐清凯 <id@xuqingkai.com>
2023-10-08 22:16:13 +0800

    Create test1.py

---
+++ test1.py
@@ -0,0 +1,8 @@
+class test11(object):
+    'SSSS' #SFDDSAFSDF
+    def __init__():
+        pass
+    def test111():
+        return "test1.test11.test111"
+    def test112():
+        return "test1.test11.test112"
