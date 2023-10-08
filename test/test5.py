cb7699b923d0a29c7fc5d6ff8c0f0c9e89579886
徐清凯 <id@xuqingkai.com>
2023-10-08 22:19:12 +0800

    Create test5.py

---
+++ test/test5.py
@@ -0,0 +1,7 @@
+class test55(object):
+    def __init__():
+        pass
+    def test551():
+        return "test.test5.test55.test551"
+    def test552():
+        return "test.test5.test55.test552"
